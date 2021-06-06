ffd4bd2b08188bcb53133cb17caa7757044fabc5
KyreOn <kireonbk@gmail.com>
2021-06-07 01:04:00 +0500

    1.1

---
+++ 1.1/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h,w;
+    cin >>h>>w;
+    cout << 2*(h+w) <<endl;
+}
---
+++ 1.1/6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,w;
+    cin >>a>>w;
+    cout << a+w<<endl;
+}
